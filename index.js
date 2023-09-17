6200c9b8e7db8031929d98bbd4ef416fd96e32c5
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2023-09-17 18:35:58 +1200

    add main() to index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Empty server file.");
+import puppeteer from "puppeteer";
+import path from 'path';
+import "dotenv/config";
+
+(async () => {
+  // Load extensions: (uBlock, )
+  const pathToExtension = path.join(process.cwd(), "./extensions/uBlock0.chromium");
+
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto("https://developer.chrome.com/");
+
+  // Set screen size
+  await page.setViewport({ width: 1920, height: 1080 });
+
+  // grab screenshot (or pdf())
+  await page.screenshot({ path: "example.png", fullPage: true });
+  //await browser.close(); //closes the browser
+})();
